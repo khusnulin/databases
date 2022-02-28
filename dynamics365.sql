6742befd6ecaabad3e2b6e642e39acd81e2cbbb5
khusnulin <30923799+khusnulin@users.noreply.github.com>
2022-02-28 14:19:36 +0000

    Update dynamics365.sql

--- dynamics365.sql
+++ dynamics365.sql
@@ -3,3 +3,9 @@ SELECT Telephone3, SUM(Revenue) as total_rev
 from [_MSCRM].[dbo].[AccountBase]
 group by Telephone3
 order by total_rev desc
+
+
+/*   Update Industry      */
+Update [_MSCRM].[dbo].[AccountBase]
+SET IndustryCode=100000062
+		Where SIC LIKE '62.%' AND IndustryCode IS NULL;
